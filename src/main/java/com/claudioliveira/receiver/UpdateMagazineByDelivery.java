20c209c6af2bc0d2f706bc3049432d2c70f68f69
Claudio Eduardo de Oliveira <claudioed.oliveira@gmail.com>
2015-07-29 12:48:37 -0300

    Improve APIs
    Change-Id: I385fad03a116e9af77a9b3d6f7d0d342a7d033fe

---
+++ src/main/java/com/claudioliveira/receiver/UpdateMagazineByDelivery.java
@@ -0,0 +1,30 @@
+package com.claudioliveira.receiver;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+
+/**
+ * @author Claudio E. de Oliveira (claudioed.oliveira@gmail.com).
+ */
+public class UpdateMagazineByDelivery extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        final MongoClient mongoClient = MongoClient.createShared(vertx,
+                new JsonObject().put("magazine-manager", "magazine-manager"), "magazine-manager");
+        EventBus eb = vertx.eventBus();
+        eb.consumer("new-delivery", message -> {
+            JsonObject entry = new JsonObject(message.body().toString());
+            JsonArray elements = entry.getJsonArray("elements");
+            elements.forEach(magazine -> mongoClient.insert("magazines", new JsonObject(magazine.toString()).put("available",Boolean.TRUE), result -> {
+                if (result.failed()) {
+                    return;
+                }
+            }));
+        });
+    }
+
+}
