7eaa3219e95431730860a3cb58ef7ce00f2c0678
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2015-07-30 23:23:00 -0300

    mailer refactory

--- src/main/java/com/claudioliveira/receiver/NewMagazineMailer.java
+++ src/main/java/com/claudioliveira/receiver/NewMagazineMailer.java
@@ -5,9 +5,13 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 
 /**
+ * 
+ * This handler is responsible to send email to customers which have the
+ * favorite magazine.
+ *  
  * @author Claudio E. de Oliveira (claudioed.oliveira@gmail.com).
  */
-public class SendEmailNewMagazine extends AbstractVerticle{
+public class NewMagazineMailer extends AbstractVerticle{
 
     public void start() throws Exception {
         final MongoClient mongoClient = MongoClient.createShared(vertx,
